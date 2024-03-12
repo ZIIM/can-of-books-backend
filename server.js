836bc73a4f489ff9d100750229ef2c203b4c6c35
John Thomas Gause III <johntgause12@gmail.com>
2024-03-11 21:27:02 -0700

    fixing??

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 'use strict';
-
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const dotenv = require('dotenv');
 // const mongoose = require('mongoose');
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-
-dotenv.config();
-
-const PORT = process.env.PORT || 3001;
-const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
 app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 
 app.get('/test', (request, response) => {
