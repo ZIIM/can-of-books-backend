eaffa59633a8a20f757729fcbed79192c7bbf98f
John Thomas Gause III <johntgause12@gmail.com>
2024-03-11 22:21:44 -0700

    mongoose???

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
@@ -11,21 +11,22 @@ const PORT = process.env.PORT || 3001;
 const DATABASE_URL = process.env.DATABASE_URL;
 
 
-app.get('/test', (request, response) => {
+app.get('/test', async (request, response) => {
 
+  let documents = await empty.find(); // added new
   response.send('test request received')
 
 })
 
-// mongoose.connect(DATABASE_URL)
-// .then(() => {
-//   app.listen(PORT, () => {
-//     console.log('Server is listening!', PORT);
-//   });
-// })
-// .catch(e => {
-//   console.log('DB Connection issue', e);
-// });
+mongoose.connect(DATABASE_URL)
+.then(() => {
+  app.listen(PORT, () => {
+    console.log('Server is listening!', PORT);
+  });
+})
+.catch(e => {
+  console.log('DB Connection issue', e);
+});
 
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+// commented out below
+// app.listen(PORT, () => console.log(`listening on ${PORT}`));
